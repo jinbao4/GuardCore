4c61762f68c8972f44d353038a40ca97dbb0ace9
jinbao4 <aitabdallahabdarrahman@gmail.com>
2026-03-09 23:03:27 +0000

    feat: add main plugin class

--- GuardCore-Paper/src/main/java/crystal/group/guardCorePaper/GuardCorePaper.java
+++ GuardCore-Paper/src/main/java/crystal/group/guardCorePaper/GuardCorePaper.java
@@ -4,14 +4,36 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GuardCorePaper extends JavaPlugin {
 
+    private static GuardCorePaper instance;
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        loadConfig();
+        registerListeners();
+        registerCommands();
+
+        getLogger().info("GuardCore enabled.")
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        instace = null();
+        getLogger().info("GuardCore disabled.")
+    }
+    private void loadConfig() {
+        saveDefaultConfig();
+    }
+
+    private void registerListeners() {
+
+    }
+
+    private void registerCommands() {
+
+    }
+
+    public static GuardCorePaper getInstance() {
+        return instance;
     }
 }
